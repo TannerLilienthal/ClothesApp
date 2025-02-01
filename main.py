78a62564d60e3cb87d036c2e922b41007faa443f
Tanner Lilienthal <89283339+TannerLilienthal@users.noreply.github.com>
2025-02-01 12:07:41 -0600

    Using FloatLayout now

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 
-class OutfitGrid(GridLayout):
+class OutfitLayout(FloatLayout):
     
     def __init__(self):
-        super(OutfitGrid, self).__init__()
+        super(OutfitLayout, self).__init__()
+
+        window_h = Window.height
+        window_w = Window.width
+
+        self.add_widget(
+            Button(
+                text = ">",
+                size_hint = (0.1, 0.1),
+                pos = (window_w / 2, window_h / 2)
+            )
+        )
+
 
 
 class MainApp(App):
     
     def build(self):
-        return OutfitGrid()
+        return OutfitLayout()
 
 MainApp().run()
